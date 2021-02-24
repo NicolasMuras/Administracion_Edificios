8b6f6d498740ab071e150fb0e65845838ba9243b
NicolasMuras <nicolasmuras@gmail.com>
2021-02-24 13:06:31 -0300

    serializer to pdf

--- apps/expensas/api/urls.py
+++ apps/expensas/api/urls.py
@@ -2,12 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from apps.expensas.api.views.expensas_views import ExpensasListCreateAPIView,ExpensasRetrieveUpdateDestroyAPIView
-from apps.expensas.views import GeneratePDF
+from apps.expensas.api.views.general_views import GeneratePDF
 
 
 urlpatterns = [
     path('list-create/', ExpensasListCreateAPIView.as_view(),name ="list-create"),
     path('retrieve-update-destroy/<int:pk>/',ExpensasRetrieveUpdateDestroyAPIView.as_view(),name ="retrieve-update-destroy"),
-    path('pdf/',GeneratePDF.as_view(),name = "pdf")
+    path('pdf/',GeneratePDF.as_view(), name = "pdf")
     
 ]
--- apps/expensas/api/views/general_views.py
+++ apps/expensas/api/views/general_views.py
@@ -1,5 +1,12 @@
 from apps.base.api import GeneralListAPIView
 from apps.expensas.api.serializers.general_serializer import EdificioSerializer, DepartamentoSerializer
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import generics
+from django.views.generic import View
+from apps.expensas.utils import render_to_pdf
+from apps.expensas.api.serializers.expensas_serializer import ExpensasSerializer
+from django.template.loader import get_template
 
 
 class EdificioSerializerListAPIVIew(GeneralListAPIView):
@@ -7,9 +14,31 @@ class EdificioSerializerListAPIVIew(GeneralListAPIView):
     def get_queryset(self):
         return self.serializer_class.Meta.model.objects.filter(status = True)
 
+
 class DepartamentoSerializerListAPIView(GeneralListAPIView):
     serializer_class = DepartamentoSerializer
     def get_queryset(self):
         return self.serializer_class.Meta.model.objects.filter(status = True)
-    
-    
+
+
+class GeneratePDF(View):
+
+    serializer_class = ExpensasSerializer
+    def get(self, request, pk = None):
+
+        template = get_template('pdf/invoice.html')
+
+        context = (self.serializer_class.Meta.model.objects.filter(status = True).values().filter(id = 7))[0]
+        print(context)
+        html = template.render(context)
+        pdf = render_to_pdf('pdf/invoice.html', context)
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Invoice_%s.pdf" %("12341231")
+            content = "inline; filename='%s'" %(filename)
+            download = request.GET.get("download")
+            if download:
+                content = "attachment; filename='%s'" %(filename)
+            response['Content-Disposition'] = content
+            return response
+        return HttpResponse("Not found")
